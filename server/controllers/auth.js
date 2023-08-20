b7e53a67dc9ca250931ad52025d1d807c1cf88e8
Elio Miguel Gonzalez Stanzione <eliostanzione@hotmail.com>
2023-08-20 11:26:38 -0500

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER*/
+export const register = async (req, res) => {
+    try {
+        const {firstName, lastName, email, password, picturePath, friends, location, occupation} = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+    } catch (err) {}
+};
